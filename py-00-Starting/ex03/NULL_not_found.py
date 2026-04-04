e20708237dd0e3cf2ec53451cc24cc7f177336e9
Ayoub Belhadj <aubbhj@gmail.com>
2026-04-04 17:08:08 +0100

    py00-ex03: Null values

---
+++ py-00-Starting/ex03/NULL_not_found.py
@@ -0,0 +1,15 @@
+def NULL_not_found(object: any) -> int:
+    if object is None:
+        print(f"Nothing: {object} {type(object)}")
+    elif type(object) is float and object != object:
+        print(f"Cheese: {object} {type(object)}")
+    elif type(object) is bool and object == False:
+        print(f"Fake: {object} {type(object)}")
+    elif type(object) is int and object == 0:
+        print(f"Zero: {object} {type(object)}")
+    elif type(object) is str and object == "":
+        print(f"Empty: {object} {type(object)}")
+    else:
+        print("Type not Found")
+        return 1
+    return 0
