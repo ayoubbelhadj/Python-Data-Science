2c9d09d49f9eaaa4ffb5278f9f0b78b006a3d158
Ayoub Belhadj <aubbhj@gmail.com>
2026-04-04 15:37:02 +0100

    py00-ex00: change remove to discard for set

--- py-00-Starting/ex00/Hello.py
+++ py-00-Starting/ex00/Hello.py
@@ -7,9 +7,9 @@ ft_dict = {"Hello": "titi!"}
 
 ft_list[1] = "World!"
 ft_tuple = ("Hello", "Morocco!")
-ft_set.remove("tutu!")
+ft_set.discard("tutu!")
 ft_set.add("Khouribga!")
-ft_dict['Hello'] = "1337!"
+ft_dict["Hello"] = "1337!"
 
 print(ft_list)
 print(ft_tuple)
