6d83c987817f733bf89ad1e268863561ea4b6f1b
Ayoub Belhadj <aubbhj@gmail.com>
2026-04-05 12:51:01 +0100

    py00-py00-ex06: Filter String

---
+++ py-00-Starting/ex06/filterstring.py
@@ -0,0 +1,25 @@
+from ft_filter import ft_filter
+import sys
+
+def main():
+    """Filter words from a string by minimum length.
+
+    Takes two command-line arguments:
+        argv[0]: a string of space-separated words
+        argv[1]: a numeric minimum length (integer)
+
+    Prints the list of words whose length is strictly greater than the given number.
+    Raises AssertionError if the argument count is wrong or the second argument is not numeric.
+    """
+    try:
+        argv = sys.argv[1:]
+        assert len(argv) == 2 , "the arguments are bad"
+        assert argv[1].isnumeric(), "the arguments are bad"
+        text = argv[0].split()
+        nbr = int(argv[1])
+        print(list(ft_filter(lambda x : len(x) > nbr , text)))
+    except AssertionError as e:
+        print(f"AssertionError: {e}")
+
+if __name__ == "__main__":
+    main()
